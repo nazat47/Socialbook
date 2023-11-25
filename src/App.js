7fbd06efe6fc0409f2f44e8a4d5fad791d09a69c
Nazat <77552644+nazat47@users.noreply.github.com>
2023-11-25 17:48:05 +0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import {
   createBrowserRouter,
+  createRoutesFromElements,
   RouterProvider,
   Route,
   Outlet,
@@ -12,63 +13,77 @@ import LeftBar from "./components/leftBar/LeftBar";
 import RightBar from "./components/rightBar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
-import './style.scss'
+import "./style.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
 import { AuthContext } from "./context/authContext";
+import { NotFound } from "./pages/notFound/NotFound";
 
 function App() {
-  const {darkMode}=useContext(DarkModeContext)
-  const {currUser}=useContext(AuthContext)
+  const { darkMode } = useContext(DarkModeContext);
+  const { currUser } = useContext(AuthContext);
   const Layout = () => {
     return (
-      <div className={`theme-${darkMode?"dark":"light"}`}>
+      <div className={`theme-${darkMode ? "dark" : "light"}`}>
         <Navbar />
-        <div style={{display:'flex'}}>
-        <LeftBar />
-        <div style={{flex:6}}> 
-        <Outlet /> 
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
+
+          <RightBar />
         </div>
-        
-        <RightBar />
-        </div>
-        
       </div>
     );
   };
 
-  const ProtectedRoute=({children})=>{
-     return currUser ? children : <Navigate to='/login' />
-  }
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <ProtectedRoute>
-        <Layout />
-      </ProtectedRoute>
-      ),
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/profile/:id",
-          element: <Profile />,
-        },
-      ],
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/register",
-      element: <Register />,
-    },
-  ]);
+  const ProtectedRoute = ({children}) => {
+    return currUser ? children : <Navigate to="/login" />;
+  };
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={
+          (<ProtectedRoute><Layout /></ProtectedRoute>)
+        }>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/profile/:id" element={<Profile />}></Route>
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />}/>
+      </Route>
+    )
+  );
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: (
+  //       <ProtectedRoute>
+  //       <Layout />
+  //     </ProtectedRoute>
+  //     ),
+  //     children: [
+  //       {
+  //         path: "/",
+  //         element: <Home />,
+  //       },
+  //       {
+  //         path: "/profile/:id",
+  //         element: <Profile />,
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     path: "/login",
+  //     element: <Login />,
+  //   },
+  //   {
+  //     path: "/register",
+  //     element: <Register />,
+  //   },
+  // ]);
 
   return (
     <div>
